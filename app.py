8339b5601e7ef34c5ffb0c6de716def701661046
xxfigueroa1993 <xxfigueroa1993@gmail.com>
2026-02-27 17:18:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Server is alive."
+    return """
+    <h1>Bright Clinical AI</h1>
+    <button onclick="test()">Test JS</button>
+    <p id="status">Idle</p>
+
+    <script>
+    function test(){
+        document.getElementById("status").innerText = "JavaScript working";
+    }
+    </script>
+    """
 
 port = int(os.environ.get("PORT", 10000))
 app.run(host="0.0.0.0", port=port)
